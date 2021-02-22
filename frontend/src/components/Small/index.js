590ab6fbd3832a82d6605a7063079cf623874c43
gdys92 <gdys92@gmail.com>
2021-02-22 14:44:16 -0300

    api

--- frontend/src/components/Small/index.js
+++
@@ -1,9 +0,0 @@
-import styled, { css } from 'styled-components'
-
-
-const StyledSmall = styled.small`
-    font-size: ${props => props.theme.small};
-    ${props => props.error && css`color: ${props => props.theme.errorText}`};
-`
-
-export default StyledSmall
--- frontend/src/containers/Sidebar/index.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux'
-import Sidebar from './Component'
-
-
-const mapStateToProps = (state) => ({
-    token: state.auth.token
-})
-
-export default connect(mapStateToProps)(Sidebar)
